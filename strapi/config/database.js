c487ea16bb4dbeb1d6d2e249da838a14af01eee6
Brian Koech <brnkoech@gmail.com>
2023-09-26 19:33:58 +0300

    Add db and server configs

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -2,7 +2,12 @@ module.exports = () => ({
   connection: {
     client: "mysql",
     connection: {
-      connectionString: process.env.DATABASE_URL,
+      // connectionString: process.env.DATABASE_URL,
+      host: process.env.AZURE_MYSQL_HOST,
+      port: process.env.AZURE_MYSQL_PORT,
+      database: process.env.AZURE_MYSQL_DATABASE,
+      username: process.env.AZURE_MYSQL_USER,
+      password: process.env.AZURE_MYSQL_PASSWORD,
     },
     useNullAsDefault: true,
   },
--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: process.env.WEBSITE_HOSTNAME,
   app: {
     keys: env.array("APP_KEYS"),
   },
